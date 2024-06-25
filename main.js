d232919452100135885836d7c20a3c94b6d149c6
eliran-positive <51043365+eliran-positive@users.noreply.github.com>
2024-06-25 17:14:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -45,14 +45,30 @@ function addRecords() {
     const temp1 = transaction.objectStore('temp1');
     const temp2 = transaction.objectStore('temp2');
 
-    temp1.add({ syncStatus: 1 }).onsuccess = () => {
+    const temp1Request = temp1.add({ syncStatus: 1 });
+    temp1Request.onsuccess = () => {
         console.log('Record added to temp1');
+    };
+    temp1Request.onerror = (event) => {
+        console.error('Error adding record to temp1:', event.target.errorCode);
     };
 
     // Delay of 10 seconds before adding to temp2
     setTimeout(() => {
-        temp2.add({ syncStatus: 1 }).onsuccess = () => {
+        const transaction2 = db.transaction(['temp2'], 'readwrite');
+        transaction2.onerror = (event) => {
+            console.error('Transaction error:', event.target.errorCode);
+        };
+        transaction2.oncomplete = (event) => {
+            console.log('Second transaction completed');
+        };
+        const temp2 = transaction2.objectStore('temp2');
+        const temp2Request = temp2.add({ syncStatus: 1 });
+        temp2Request.onsuccess = () => {
             console.log('Record added to temp2');
+        };
+        temp2Request.onerror = (event) => {
+            console.error('Error adding record to temp2:', event.target.errorCode);
         };
     }, 10000);
 }
@@ -74,13 +90,32 @@ function addRecordsWithException() {
     const temp1 = transaction.objectStore('temp1');
     const temp2 = transaction.objectStore('temp2');
 
-    temp1.add({ syncStatus: 1 }).onsuccess = () => {
+    const temp1Request = temp1.add({ syncStatus: 1 });
+    temp1Request.onsuccess = () => {
         console.log('Record added to temp1');
+    };
+    temp1Request.onerror = (event) => {
+        console.error('Error adding record to temp1:', event.target.errorCode);
     };
 
     // Delay of 10 seconds before adding to temp2 and then throw an exception
     setTimeout(() => {
-        console.log('Throwing an exception after delay');
-        throw new Error('Intentional error after delay');
+        const transaction2 = db.transaction(['temp2'], 'readwrite');
+        transaction2.onerror = (event) => {
+            console.error('Transaction error:', event.target.errorCode);
+        };
+        transaction2.oncomplete = (event) => {
+            console.log('Second transaction completed');
+        };
+        const temp2 = transaction2.objectStore('temp2');
+        const temp2Request = temp2.add({ syncStatus: 1 });
+        temp2Request.onsuccess = () => {
+            console.log('Record added to temp2');
+            console.log('Throwing an exception after delay');
+            throw new Error('Intentional error after delay');
+        };
+        temp2Request.onerror = (event) => {
+            console.error('Error adding record to temp2:', event.target.errorCode);
+        };
     }, 10000);
 }
